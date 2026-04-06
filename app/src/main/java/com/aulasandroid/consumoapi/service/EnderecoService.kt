34e4c95e44eb6462269930f0ae64a8d5ab63cf54
SIDNEY CAMPOS ARAGÃO <25132876@dsn127m1246730.cfp127aluno.local>
2026-04-06 16:54:45 -0300

    Api

---
+++ app/src/main/java/com/aulasandroid/consumoapi/service/EnderecoService.kt
@@ -0,0 +1,20 @@
+package com.aulasandroid.consumoapi.service
+
+import com.aulasandroid.consumoapi.model.Endereco
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface EnderecoService {
+
+    @GET("{cep}/json/")
+    fun getEnderecoByCep(@Path("cep") cep: String ): Call<Endereco>
+
+
+    @GET("{uf}/{cidade}/{rua}/json")
+    fun getEnderecosByUfCidadeRua(
+        @Path("uf") uf: String,
+        @Path("cidade") cidade: String,
+        @Path("rua") rua: String
+    ): Call<List<Endereco>>
+}
